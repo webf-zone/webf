a2904d0d3666cd59820e68085c77157c3736d637
Harshal Patil <patilh3@mozy.com>
2015-06-26 02:17:22 +0530

    Corrected bug in deploy script

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     rsync -rv --exclude=.git  ../$DIST_FOLDER/* .
 	#rsync -r --exclude=.git --delete ../$DIST_FOLDER/ ./
     #add, commit and push files
-    git add -all
+    git add --all
     git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
     git push -fq origin $BRANCH > /dev/null
     echo -e "Deploy completed\n"
